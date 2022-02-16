44d1a88c076c81e5307d99653b589a95eecbaf18
Thành Đinh <phuocthanh2409@gmail.com>
2022-02-16 16:19:08 +0700

    Delete websitenuochoa directory

--- websitenuochoa/libraries/connect.php
+++
@@ -1,4 +0,0 @@
-<?php
-     $o = new PDO("mysql:host=localhost; dbname=nuochoa",'root' ,'' );
-     $o->query('set names utf8');
-?>
--- websitenuochoa/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-include('connect.php');
-$session_userId = '';
-$_SESSION['userId'] = '';
-if (empty($session_userId) && empty($_SESSION['userId'])) {
-    $url = BASE_URL . 'formGui.php';
-    header("Location: $url");
-}
